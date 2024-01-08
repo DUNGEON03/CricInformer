12c3a17f05d7351c92717d51c63d8014a39a89ea
Udit Sonkar <60100573+DUNGEON03@users.noreply.github.com>
2024-01-08 14:52:05 +0530

    Add files via upload

---
+++ src/test/java/com/cric/apis/CricInformer/CricInformerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cric.apis.CricInformer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CricInformerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
